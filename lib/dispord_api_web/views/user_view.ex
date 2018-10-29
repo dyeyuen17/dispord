b1d7e5d83f593ca69a62cc68ae1c3e0d5a885020
dyey <dyeyuen17@gmail.com>
2018-10-29 15:53:03 +0800

    added view for user

---
+++ lib/dispord_api_web/views/user_view.ex
@@ -0,0 +1,20 @@
+defmodule DispordApiWeb.UserView do
+  use DispordApi, :view
+
+  def render("show.json", %{users: users}) do
+    %{data: render_many(users, __MODULE__, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{
+      id: user.id,
+      email: user.email,
+      username: user.username,
+      setup_password_token: user.setup_password_token,
+      reset_password_token: user.reset_password_token,
+      email_verified: user.email.verified,
+      inserted_at: user.inserted_at,
+      updated_at: user.updated_at
+    }
+  end
+end
